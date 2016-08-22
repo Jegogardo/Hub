ffcad1db911e91d0005041d6de15f7a4ed3f31d4
Jegogardo <iweek16@gmail.com>
2016-08-22 19:12:38 +0200

    add features

--- es6.js
+++ es6.js
@@ -1,6 +1,10 @@
 const ROOT = "../../"
+const CALLBACKS = [ "onsuccess", "ondone", "onstart", "onerror" ]
 //const READYSTATE = [0,1,2,3,4]
 //const STATUS
+
+
+
 class Bhv{
 
     static testStr( str ){
@@ -8,22 +12,30 @@ class Bhv{
     }
 
 
-
-    set whereami ( str = null ) {
-        if( str == null )
-            str = "I'm here!!!" 
-        this.debug_str = str
-        console.log( this.debug_str )
-        }
-    get whereami () { console.log( this.debug_str || "I'm here!!!" )}
+    static whereami( str ){
+        console.log( str || "I'm here!!!" );
+    }
 
     get debug () { console.log( this ) }
+    set debug (obj) {
+        console.log(obj) }
     
 
 }
 
 class Hub extends Bhv {
 
+
+    get url(){ return this._url }
+    set url( url ){
+        if( typeof url != "string" ){
+            console.error("URL is not valid")
+            return 
+        }
+        else
+            this._url = url
+            
+    }
     
 
     get method(){ return this._method;}
@@ -38,54 +50,101 @@ class Hub extends Bhv {
     get queryString(){ return this._queryString }
     set queryString( param ){
         this._queryString = "";
-        if( typeof param === "object" ){
-            var ampersand = "";
-            for(let i in param){
-                this._queryString += `${ampersand}${i}=${param[i]}`
 
-                var ampersand = "&";
+        if( param != null ){
+
+            if( typeof param === "object" ){
+                var ampersand = "";
+                for(let i in param){
+                    this._queryString += `${ampersand}${i}=${param[i]}`
+
+                    var ampersand = "&";
+                }
+
             }
 
-        }
-
-        if( this.method == "GET" )
-             if( param.startsWith( "?" ) )
-                console.info( "Not need to start the queryString with '?'" );
+            if( this.method == "GET" )
+                if( param.startsWith( "?" ) )
+                    console.info( "Not need to start the queryString with '?'" );
+                else
+                    this._queryString = "?"+param;
             else
-                this._queryString = "?"+param;
-        else
-            if( param.startsWith( "?" ) ){
-                this._queryString = param.substring(1);
-                console.info( "Not need to start the queryString with '?'" );
+                if( param.startsWith( "?" ) ){
+                    this._queryString = param.substring(1);
+                    console.info( "Not need to start the queryString with '?'" );
+                }
+                    
+
             }
-                
-
-
+        
         return this._queryString;
     }
 
     get async(){ return this._async }
     set async( isAsync ){ isAsync? this._async = true: this._async = false  }
+
+    get onerror(){ return this._onerror }
+    set onerror( fn ){ this._onerror = fn }
+
+    get onsuccess(){ return this._onsuccess }
+    set onsuccess( fn ){ this._onsuccess = fn }
+
+    get onstart(){ return this._onstart }
+    set onstart( fn ){ this.req.onloadstart = this._onstart =  fn }
+
+    get ondone(){ return this._ondone }
+    set ondone( fn ){ this._ondone = fn }
+
+    get onprogress(){ return this._onprogress }
+    set onprogress( fn ){ this.req.onprogress = this._onprogress = fn }
     
-    constructor( url, method = "GET", param = null, async = true ) {
+    
+    constructor( url, method, param, async, callbacks = {} ) {
+        
         super()
-        this.url = url;
+
+        if( typeof method == "object" ){
+            debugger
+            method = "GET"
+        }
+            
+        
+        this.url = url || null
+        this.method = method || "GET"
+        this.queryString = param || null
+        this.async = async || true
         this.req = new XMLHttpRequest()
-        this.method = method
-        this.queryString = param;
-        this.async = async;
+        this._onerror = null
+        this._onsuccess = null
+        this._onstart = null
+        this._ondone = null
+        this.req.onreadystatechange = this.statechange.bind(this)
                 
     }
 
-    abort(){
 
-    }
 
-    success(){
+    statechange( e ){
+        //debugger
+        let req = e.target;
+        if( req.readyState == req.DONE){
+            this.result =  {
+                    response: this.req.response,
+                    responseText: this.req.responseText,
+                    responseType: this.req.responseType,
+                    responseURL: this.req.responseURL,
+                    responseXML: this.req.responseXML
+                                }
 
-    }
-
-    response(){
+            if( req.status == 200 && this.onsuccess )
+                this.onsuccess( this.result )
+            if( req.status == 404 && this.onerror )
+                this.onerror( this.result  )
+            if( this.ondone )
+                this.ondone( this.result )
+                
+        }
+            
         
     }
 
@@ -95,28 +154,35 @@ class Hub extends Bhv {
     }
 
     connect(){
-        if( this.method == "GET" ){
-            this.req.open( "GET", this.url+this.queryString, this.async );
-            this.req.send();
-        }
-        else{
-            this.req.open( "POST", this.url, this.async );
-            if( this.req.getAllResponseHeaders() == "" ){
-                this.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        if( this.url ){
+            if( this.method == "GET" ){
+                this.req.open( "GET", this.url+this.queryString, this.async );
+                this.req.send();
             }
+            else{
+                this.req.open( "POST", this.url, this.async )
+                if( this.req.getAllResponseHeaders() == "" ){
+                    this.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+                }
 
-            this.req.send( this.queryString );
+                this.req.send( this.queryString )
 
-            return this;
+                
+            }
         }
+        else
+            console.warn("Needs an URL to make a request");
+
+        return this
             
     }
 
     static connect( ...param ){
         //debugger
-        var [url, method, param, async] = param;
-        let temp = new this( url, method, param, async ).connect();
+        var [url, method, param, async] = param
+        let temp = new this( url, method, param, async ).connect()
 
+        return temp
                 
     }
 
@@ -125,7 +191,40 @@ class Hub extends Bhv {
 }
 
 //let hub = new Hub( "prova","GET",{data:"",t:"c"} );
-let hub = new Hub( `${ROOT}index.php`,"POST", "data=&t=c" ).connect();
-//Hub.connect( `${ROOT}index.php`,"POST", "data=&t=c" );
-Hub.connect( `${ROOT}index.php`,"POST", "data=&t=c" )
-hub.debug;
+/*let hub = new Hub( `${ROOT}inde.php`,"POST", "data=&t=c" );
+hub.onerror = (result)=>{
+    console.error( "Errore" );
+}
+hub.onsuccess = ( result ) =>{
+    console.log( "Successo" );
+}
+hub.onstart = () =>{
+    console.info( "Iniziato" );
+}
+hub.ondone = (result) =>{
+    console.info( "Finito");
+}*/
+
+
+
+/*var i=1;
+hub.onprogress = () =>{
+    console.info( `Progress ${i++}` );
+}*/
+
+//hub.connect()
+/*let hub = new Hub( `${ROOT}index.php`,"POST", {
+                                                onsuccess : ( result ) =>{ console.log(result.response) },
+                                                ondone : ( result ) =>{ console.log("end") },
+                                                onstart : ( result ) =>{ console.log("start") }
+                                                })
+*/
+let hub2 = new Hub( `${ROOT}index.php`,
+                    {
+                    onsuccess : ( result ) =>{ console.log(result.response) },
+                    ondone : ( result ) =>{ console.log("end") },
+                    onstart : ( result ) =>{ console.log("start") }
+                    })
+                                                     
+
+//hub.debug;
