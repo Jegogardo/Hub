0496ec631eda6705fa702ac1a0d826efd20c496d
Jegogardo <iweek16@gmail.com>
2016-08-22 11:55:48 +0200

    fixbug

--- es6.js
+++ es6.js
@@ -18,16 +18,18 @@ class Bhv{
 
 class Hub extends Bhv {
 
+    get method(){ return this._method;}
     set method ( m )  {
+        
         if(m === "GET" || m === "POST")
             this._method = m
         else
             console.error("Method can be set to GET or POST value");
         }
 
-    get method(){ return this._method;}
-
-    get queryString(){ return this.queryString }
+    
+    
+    get queryString(){ return this._queryString }
     set queryString( param ){
         this._queryString = "";
         if( typeof param === "object" ){
@@ -39,30 +41,59 @@ class Hub extends Bhv {
             }
 
         }
-        
 
         if( this.method == "GET" )
              if( param.startsWith( "?" ) )
                 console.info( "Not need to start the queryString with '?'" );
             else
-                this._queryString = "?"+this._queryString;
+                this._queryString = "?"+param;
         else
+            if( param.startsWith( "?" ) ){
+                this._queryString = param.substring(1);
+                console.info( "Not need to start the queryString with '?'" );
+            }
+                
 
 
         return this._queryString;
     }
+
+    get async(){ return this._async }
+    set async( isAsync ){ isAsync? this._async = true: this._async = false  }
     
-    constructor( url, method="GET", param=null ) {
+    constructor( url, method = "GET", param = null, async = true ) {
         super()
         this.req = new XMLHttpRequest()
         this.method = method
         this.queryString = param;
+        this.async = async;
         
         
+    }
+
+    setRequestHeader( header ,value ){
+        this.req.setRequestHeader( header, value );
+    }
+
+    connect(){
+        if( this.method == "GET" ){
+            this.req.open( "GET", this.url+this.queryString, this.async );
+            this.req.send();
+        }
+        else{
+            this.req.open( "POST", this.url, this.async );
+            if( this.req.getAllResponseHeaders() == "" ){
+                this.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            }
+
+            
+            this.req.send( this.queryString );
+        }
+            
     }
 
 }
 
 //let hub = new Hub( "prova","GET",{data:"",t:"c"} );
-let hub = new Hub( "prova","GET", "data=&t=c" );
+let hub = new Hub( "localhost/index.php","POST", "data=&t=c" );
 hub.debug;
